1e8e8934f3958fb2fadc42e38b6a1f87d26cedfd
Jaykadam04 <135636175+Jaykadam04@users.noreply.github.com>
2025-03-25 20:59:12 +0530

    Add files via upload

---
+++ factorial.java
@@ -0,0 +1,13 @@
+public class factorial {
+    public static void main(String[] args) {
+        int num = 5; // Change this number as needed
+        long factorial = 1;
+
+        for (int i = 1; i <= num; i++) {
+            factorial *= i;
+        }
+        System.out.println("Jay Kadam ");
+
+        System.out.println("Factorial of " + num + " is: " + factorial);
+    }
+}
---
+++ factorial.py
@@ -0,0 +1,10 @@
+def factorial(n):
+    result = 1
+    for i in range(1, n + 1):
+        result *= i
+    return result
+
+num = 5  # Change this number as needed
+
+print("Jay Kadam in python")
+print(f"Factorial of {num} is: {factorial(num)}")
